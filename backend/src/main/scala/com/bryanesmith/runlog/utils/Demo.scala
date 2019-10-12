9e130d3750cdb05d887ec5d4d82baac5f6073b80
Bryan Smith <bryanesmith@gmail.com>
2019-10-12 11:35:31 -0400

    log today's exercise

--- backend/src/main/scala/com/bryanesmith/runlog/utils/Demo.scala
+++ backend/src/main/scala/com/bryanesmith/runlog/utils/Demo.scala
@@ -8,6 +8,21 @@ object Demo {
 
   def events = Seq(
     Event(
+      atId = "_:341",
+      atType = Type.RunCrossTrain,
+      date = "2019-10-12",
+      runData = Some(
+        Intervals(
+          category = IntervalsCategory.Intervals,
+          count = 6,
+          intervalDuration = Some("PT1M"),
+          intervalSpeed = Some(9.1),
+          restDuration = Some("PT1M")
+        )
+      ),
+      notes = Some("1.99mi + lift")
+    ),
+    Event(
       atId = "_:340",
       atType = Type.Run,
       date = "2019-10-06",
