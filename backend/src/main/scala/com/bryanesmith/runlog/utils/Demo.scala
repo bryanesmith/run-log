9e4b043a25bfb24e41e17654a8a384f94e3bd289
Bryan Smith <bryanesmith@gmail.com>
2019-10-24 20:17:06 -0400

    log today's exercise

--- backend/src/main/scala/com/bryanesmith/runlog/utils/Demo.scala
+++ backend/src/main/scala/com/bryanesmith/runlog/utils/Demo.scala
@@ -8,6 +8,17 @@ object Demo {
 
   def events = Seq(
     Event(
+      atId = "_:346",
+      atType = Type.Run,
+      date = "2019-10-24",
+      runData = Some(
+        SteadyStateRun(
+          category = SteadyStateRunCategory.Casual,
+          distance = Some(8.53)
+        )
+      )
+    ),
+    Event(
       atId = "_:345",
       atType = Type.CrossTrain,
       date = "2019-10-23"
