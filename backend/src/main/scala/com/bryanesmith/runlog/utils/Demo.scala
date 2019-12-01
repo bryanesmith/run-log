d7daf1ca8ff2213a352cf2ee9c66f3526e6e346b
Bryan Smith <bryanesmith@gmail.com>
2019-12-01 10:51:13 -0500

    logging 11/05 crosstraining

--- backend/src/main/scala/com/bryanesmith/runlog/utils/Demo.scala
+++ backend/src/main/scala/com/bryanesmith/runlog/utils/Demo.scala
@@ -8,6 +8,11 @@ object Demo {
 
   def events = Seq(
     Event(
+      atId = "_:350",
+      atType = Type.CrossTrain,
+      date = "2019-11-05"
+    ),
+    Event(
       atId = "_:349",
       atType = Type.Run,
       date = "2019-11-03",
