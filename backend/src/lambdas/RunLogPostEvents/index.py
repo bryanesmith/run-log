f9000fda3b0cb71c77e8fa8e6e81ab49f1f9b0b8
Bryan Smith <bryanesmith@gmail.com>
2019-12-29 17:00:02 -0500

    RunLogPostEvents used to create or update event

--- backend/src/lambdas/RunLogPostEvents/index.py
+++ backend/src/lambdas/RunLogPostEvents/index.py
@@ -9,7 +9,11 @@ def lambda_handler(event, context):
     print("Total number of events to create: {}".format(len(events)))
 
     for run_event in events:
-        add_run_event(table, run_event)
+
+        # DynamoDB doesn't support Python floats: https://github.com/boto/boto3/issues/534#issuecomment-191960592
+        run_event = lambda_utils.replace_floats(run_event)
+
+        table.put_item(Item=run_event)
 
     respBody = {
         'statusCode': 200,
@@ -17,21 +21,3 @@ def lambda_handler(event, context):
     }
 
     return lambda_utils.addCORS(event, respBody)
-
-def add_run_event(table, run_event):
-
-    key = '@id'
-
-    # DynamoDB doesn't support Python floats: https://github.com/boto/boto3/issues/534#issuecomment-191960592
-    run_event = lambda_utils.replace_floats(run_event)
-
-    id = run_event[key]
-
-    # Check before adding; cannot have two events with same ID
-    item = lambda_utils.find_item_by_key(table, key, id)
-
-    if item != None:
-        print('Item already exists: {}'.format(id))
-    else:
-        print('Adding item: {}'.format(id))
-        table.put_item(Item=run_event)
